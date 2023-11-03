d119d333dd13730afe1abfe24a1f1ae7c3ab4798
hp <emmanueldusengimana83@gmail.com>
2023-11-03 15:27:12 +0200

    jikj

--- src/router/productrouter.js
+++ src/router/productrouter.js
@@ -1,7 +1,8 @@
 
 import express from "express"
 import productcontroller from "../controller/productcontroller"
+import Verifyaccess from "../middleware/velifyaccess"
 
 const router = express.Router()
-router.post("/",productcontroller.postproduct)
+router.post("/",Verifyaccess("admin"),productcontroller.postproduct)
 export default router
