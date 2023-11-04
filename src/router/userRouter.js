d60f5b8d68ab6dce6af06bc97636b16efe97f3f7
hp <emmanueldusengimana83@gmail.com>
2023-11-04 10:26:27 +0200

    ghh

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -8,5 +8,5 @@ router.delete("/",usercontroller.deleteall)
 router.post("/login",usercontroller.Login)
 router.get("/get",Verifyaccess("admin"),usercontroller.getAll)
 router.get("/:id",Verifyaccess("admin"),usercontroller.getone)
-router.patch("/update",usercontroller.updateuser)
+router.patch("/update/:id",usercontroller.updateuser)
 export default router
